8486fb1bcc70aedc6108ab36e2485e8b07870c09
B1429053 <fionzhang415@gmail.com>
2025-09-18 15:41:25 +0800

    Update main1.c

--- main1.c
+++ main1.c
@@ -2,10 +2,7 @@
 int main(){ 
     char a=5;
     printf("\t%d\n",a&1);
-    //0101&0001=0001
     printf("\t%d\n",a&2);
-   //0101&0010=0000
-   printf("\t%d\n",a|2);
-   //0101|0010=0111
-   return 0;
+    printf("\t%d\n",a|2);
+    return 0;
 }
