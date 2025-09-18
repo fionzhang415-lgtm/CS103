d37a2afaf4d488b5a6caedfd1c2d1a467de6a44c
B1429053 <fionzhang415@gmail.com>
2025-09-18 15:47:23 +0800

    Update main4.c

--- main4.c
+++ main4.c
@@ -3,7 +3,7 @@
     int a=10;
     if(a==10){ 
        printf("a is equal to 10\n");
-    }else if(a2<20){
+    }else if(a<20){
         printf("a is less than 20\n");
     }else{
         printf("a is not less than 20\n");
